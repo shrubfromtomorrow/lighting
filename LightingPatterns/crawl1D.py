12077f80770620c2b59909c8288e1d03c7932a83
shrubfromtomorrow <orionjandrews@gmail.com>
2022-10-01 20:20:32 -0700

    Due to now having strip lights, I made some 1D patterns and the most fleshed out of them all is the crawl, I am also using this for stress testing

--- LightingPatterns/crawl1D.py
+++ LightingPatterns/crawl1D.py
@@ -1,9 +1,6 @@
 import board
 import neopixel
 from time import sleep
-from random import randint
-import sys
-import math
 pixels = neopixel.NeoPixel(board.D18, 300, brightness = 1, auto_write = False, pixel_order = neopixel.RGB)
 
 
--- hertz.py
+++ hertz.py
@@ -3,22 +3,24 @@ import neopixel
 from time import sleep
 from random import randint
 import time
-pixels = neopixel.NeoPixel(board.D18, 200, brightness = 1, auto_write = False, pixel_order = neopixel.RGB)
+pixels = neopixel.NeoPixel(board.D18, 300, brightness = 1, auto_write = False, pixel_order = neopixel.RGB)
 pixels.fill((0, 0, 0))
 loops = 0
 st = time.time()
 while loops < 1000:
     for pixel in range(len(pixels)):
-        randR = (255, 0, 0)
-        randG = (255, 0, 0)
-        randB = (255, 0, 0)
+        randR = randint(0, 255)
+        randG = randint(0, 255)
+        randB = randint(0, 255)
         pixels[pixel] = (randR, randG, randB)
     pixels.show()
+    
     loops += 1
 et = time.time()
 elapsedTime = et - st
 print(f"Elapsed time: {elapsedTime} seconds")
 
+#300 lights exec time is 28.571308 sec
 #200 lights exec time is 19.48963 sec
 #100 lights exec time is 10.3855 sec
 #50 lights exec time s 5.29201 sec
