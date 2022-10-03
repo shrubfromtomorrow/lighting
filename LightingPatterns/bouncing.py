cf9350cdbec44a6bf498bd9e7d119ce113164042
shrubfromtomorrow <orionjandrews@gmail.com>
2022-10-03 11:48:19 -0700

    Added functions to allow modularity in the pattern with fwdColor, bkwColor, fwdSkip, bkwSkip, sleepTime. I also created some 'interlacing' by only showing lights every x number of lights updated

--- LightingPatterns/bouncing.py
+++ LightingPatterns/bouncing.py
@@ -1,23 +1,44 @@
 import board
 import neopixel
+from random import choice
 from time import sleep
-pixels = neopixel.NeoPixel(board.D18, 300, brightness = 0.3, auto_write = False, pixel_order = neopixel.GRB)
+pixels = neopixel.NeoPixel(board.D18, 300, brightness = 0.05, auto_write = False, pixel_order = neopixel.GRB)
 
-j = 0
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+teal = (0, 255, 255)
+purple = (255, 0, 255)
 
-i = 0
-while True:
+colorList = [white, red, green, blue, yellow, teal, purple]
 
-    if j % 2 == 0:
-        while i < 299:
-            if i % 1 == 0:
-                pixels[i] = (0, 100, 100)
-                pixels.show()
-            i += 1
-    else:
-        while i >= 1:
-            if i % 1 == 0:
-                pixels[i] = (0, 0, 0)
-                pixels.show()
-            i -= 1
-    j += 1
+
+
+def bounce(fwdColor, bkwColor, fwdSkip, bkwSkip, sleepTime):
+    j = 0
+
+    i = 0
+    while True:
+
+        if j % 2 == 0:
+            while i < 299:
+                if i % fwdSkip == 0:
+                    pixels[i] = choice(colorList)
+                    sleep(sleepTime)
+                if i % 5 == 0:
+                    pixels.show()
+                i += 1
+        else:
+            while i >= 1:
+                if i % bkwSkip == 0:
+                    pixels[i] = bkwColor
+                    sleep(sleepTime)
+                if i % 5 == 0:
+                    pixels.show()
+                i -= 1
+        j += 1
+
+bounce(white, black, 1, 1, 0)
